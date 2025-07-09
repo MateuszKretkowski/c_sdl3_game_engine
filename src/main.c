634269d6f5a10c29cb1273fbb7ba6e356728947d
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-07-09 13:37:20 +0200

    Initializing SDL3 and SDL_GL_Context;

--- src/main.c
+++ src/main.c
@@ -3,20 +3,60 @@
 #include <stdint.h>
 
 #include <SDL3/SDL.h>
-
-#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
+#include <GL/gl.h>
 
 #include "config.h"
 
-bool isRunning;
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv);
-
-int main(int argc, char *argv[])
-{
-    while (isRunning) {
-
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Error SDL_Init: %s\n", SDL_GetError());
+        return 1;
     }
+
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "atiEngine",
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+    if (!window) {
+        fprintf(stderr, "Error SDL_CreateWindow: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (!gl_context) {
+        fprintf(stderr, "Error SDL_GLContext: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_GL_SetSwapInterval(1); // VSync = ON
+
+    int running = 1;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = 0;
+            }
+        }
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
