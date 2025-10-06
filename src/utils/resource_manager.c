97ec5ae7a658a06ba84d05a24c8c97df5084c9ac
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-10-06 12:17:01 +0200

    Modified: utils.h: typedef struct Mesh now has double-pointer list of materials; Added: resource_manager.c/.h; Added: asset_index.json;

---
+++ src/utils/resource_manager.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <libs/cJSON.h>
+#include <utils/utils.h>
+
+Material *resource_get_material(const char* material_id) {
+    // get material path
+    // get material by path
+}
