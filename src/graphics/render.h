859a6296ab817679c282fc3f87c870cf38029ff5
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-10-23 16:54:58 +0200

    render.h: added new functions;

--- src/graphics/render.h
+++ src/graphics/render.h
@@ -1,10 +1,16 @@
 #ifndef RENDER_H
 #define RENDER_H
 
+#include "resource_manager.h"
+#include "core/gameObject.h"
+
 #define MAX_OBJECTS 2048
 
 void render_init(void);
 void render_frame(void);
 void render_shutdown(void);
+void render_load_scene(Scene *scene);
+void render_add_object(GameObject *gameObject);
+void render_remove_object(GameObject *gameObject);
 
 #endif
