ae13b87e32790738cbaa2c32fe6052e1c487f37c
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-11-17 22:45:03 +0100

    Adding/Removing objects from scene/render_stack now happens in scene_manager.c.h, which helps making the project clean and understantable;

--- src/engine/scene_manager.c
+++ src/engine/scene_manager.c
@@ -2,13 +2,33 @@
 #include <stdlib.h>
 #include "scene_manager.h"
 
-const Scene *current_scene;
+Scene *current_scene;
+camera_component *active_camera;
 
 void scene_manager_init() {
     current_scene = NULL;
+    active_camera = NULL;
+}
+
+camera_component *render_get_active_camera() {
+    for (int i=0; i<current_scene->gameObjects_length; i++) {
+        if (current_scene->gameObjects[i].id && current_scene->gameObjects[i].id && strcmp(current_scene->gameObjects[i].id, "camera") == 0) {
+            camera_component *cc = get_component(&current_scene->gameObjects[i], camera_component, "camera_component");
+            if (cc) {
+                active_camera = cc;
+                fprintf(stderr, "Active camera found: %s\n", current_scene->gameObjects[i].name);
+                return cc;
+            }
+        }
+    }
+    fprintf(stderr, "ERROR: No active camera found in scene!\n");
+    return NULL;
 }
 
 void load_scene(char *id) {
+    if (!current_scene) {
+        scene_manager_init();
+    }
     if (!id) {
         fprintf(stderr, "load_scene: id is NULL\n");
         return;
@@ -21,17 +41,37 @@ void load_scene(char *id) {
     }
 
     if (current_scene) {
+        // Clear render stack before freeing old scene
+        render_clear_stack();
         for (int i=0; i<current_scene->gameObjects_length; i++) {
-            free_gameObject(&scene->gameObjects[i]);
+            free_gameObject(&current_scene->gameObjects[i]);
         }
     }
     current_scene = scene;
-    for (int i=0; i<current_scene->gameObjects_length; i++) {
-        render_object *ro;
-        ro->gameObject = &current_scene->gameObjects[i];
-        ro->mesh_renderer_component = get_component(&current_scene->gameObjects[i], mesh_renderer_component, "mesh_renderer_component");
-        ro->transform_component = get_component(&current_scene->gameObjects[i], transform_component, "transform_component");
-        
+
+    // Resolve camera target references before awake/start
+    fprintf(stderr, "load_scene: Resolving camera target references\n");
+    for (int i=0; i<scene->gameObjects_length; i++) {
+        camera_component *cam = get_component(&scene->gameObjects[i], camera_component, "camera_component");
+        if (cam && cam->target_id) {
+            fprintf(stderr, "load_scene: Found camera with target_id '%s'\n", cam->target_id);
+            // Find the target GameObject by ID
+            for (int j=0; j<scene->gameObjects_length; j++) {
+                if (scene->gameObjects[j].id && strcmp(scene->gameObjects[j].id, cam->target_id) == 0) {
+                    transform_component *target_transform = get_component(&scene->gameObjects[j], transform_component, "transform_component");
+                    if (target_transform) {
+                        cam->target = target_transform;
+                        fprintf(stderr, "load_scene: Resolved camera target to GameObject '%s'\n", scene->gameObjects[j].id);
+                    } else {
+                        fprintf(stderr, "ERROR: Target GameObject '%s' has no transform_component\n", cam->target_id);
+                    }
+                    break;
+                }
+            }
+            if (!cam->target) {
+                fprintf(stderr, "ERROR: Camera target '%s' not found in scene\n", cam->target_id);
+            }
+        }
     }
 
     for (int i=0; i<scene->gameObjects_length; i++) {
@@ -51,6 +91,12 @@ void load_scene(char *id) {
             }
         }
     }
-
-    render_load_scene(scene);
-}
+    
+    for (int i=0; i<current_scene->gameObjects_length; i++) {
+        render_object *ro = malloc(sizeof(render_object));
+        ro->gameObject = &current_scene->gameObjects[i];
+        ro->mesh_renderer_component = get_component(&current_scene->gameObjects[i], mesh_renderer_component, "mesh_renderer_component");
+        ro->transform_component = get_component(&current_scene->gameObjects[i], transform_component, "transform_component");
+        render_add_object(ro);
+    }
+}     
--- src/graphics/render.h
+++ src/graphics/render.h
@@ -2,17 +2,25 @@
 #define RENDER_H
 
 #include "core/gameObject.h"
+#include "components/components.h"
+#include "scene_manager.h"
 
 #define MAX_OBJECTS 2048
 
-// Forward declaration - full definition in scene_manager.h
 typedef struct Scene Scene;
+
+typedef struct {
+    GameObject *gameObject;
+    mesh_renderer_component *mesh_renderer_component;
+    transform_component *transform_component;
+} render_object;
 
 void render_init(void);
 void render_frame(void);
 void render_shutdown(void);
-void render_load_scene(Scene *scene);
-void render_add_object(GameObject *gameObject);
-void render_remove_object(GameObject *gameObject);
+void render_clear_stack(void);
+void render_load_render_stack(Scene *scene);
+void render_add_object(render_object *ro);
+void render_remove_object(render_object *ro);
 
 #endif
