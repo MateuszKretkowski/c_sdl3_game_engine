fefc7c74bc67b383e6e33ce00cd126f790fb208f
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-11-20 19:26:42 +0100

    box_collider_component: is_point_inside_AABB();

--- src/components/box_collider_component.c
+++ src/components/box_collider_component.c
@@ -11,33 +11,53 @@
 #include <cglm/cglm.h>
 
 void box_collider_awake(Component* self) {
-    box_collider_component *comp = (sample_component*)self;
+    box_collider_component *comp = (box_collider_component*)self;
 }
 
 void box_collider_start(Component* self) {
-    box_collider_component *comp = (sample_component*)self;
+    box_collider_component *comp = (box_collider_component*)self;
 }
 
 void box_collider_update(Component* self) {
-    box_collider_component *comp = (sample_component*)self;
+    box_collider_component *comp = (box_collider_component*)self;
 }
 
 void box_collider_destroy(Component* self) {
-    box_collider_component *comp = (sample_component*)self;
+    box_collider_component *comp = (box_collider_component*)self;
 }
 
-box_collider_component *create_sample_component() {
+bool is_point_inside_AABB(box_collider_component *comp, Vector3 pos) {
+    Vector3 half = {
+        comp->scale.x * 0.5f,
+        comp->scale.y * 0.5f,
+        comp->scale.z * 0.5f
+    };
+
+    return (
+        pos.x >= comp->pos.x - half.x &&
+        pos.x <= comp->pos.x + half.x  &&
+        pos.y >= comp->pos.y - half.y  &&
+        pos.y <= comp->pos.y + half.y  &&
+        pos.z >= comp->pos.z - half.z  &&
+        pos.z <= comp->pos.z + half.z 
+    );
+}
+
+box_collider_component *create_box_collider_component(Vector3 pos, Vector3 scale) {
     box_collider_component* comp = malloc(sizeof(box_collider_component));
-    comp->base.id = strdup("sample_component");
-    comp->base.name = strdup("Sample");
+    comp->base.id = strdup("box_collider_component");
+    comp->base.name = strdup("box_collider");
     comp->base.isActive = true;
     comp->base.gameObject = NULL;
-    comp->base.size = sizeof(sample_component);
+    comp->base.size = sizeof(box_collider_component);
     comp->base.standard_voids = malloc(sizeof(component_standard_voids));
     comp->base.standard_voids->awake = box_collider_awake;
     comp->base.standard_voids->start = box_collider_start;
     comp->base.standard_voids->update = box_collider_update;
     comp->base.standard_voids->destroy = box_collider_destroy;
+
+    comp->pos = pos;
+    comp->scale = scale;
 
     return comp;
 }
--- src/components/box_collider_component.h
+++ src/components/box_collider_component.h
@@ -7,7 +7,11 @@
 typedef struct {
     Component base;
 
+    Vector3 pos;
+    Vector3 scale;
 } box_collider_component;
+
+bool is_point_inside_AABB(box_collider_component *comp, Vector3 pos);
 
 box_collider_component *create_box_collider_component();
 
