4299d27beb5df4ee3d22c89ffd34ceff1c0fc7ff
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-11-21 16:03:27 +0100

    box_collider_component.c/.h: Changed now to according function names; sphere_collider_component.c/.h: added intersect_AABB_sphere(); intersect_sphere_sphere();

--- src/components/sphere_collider_component.h
+++ src/components/sphere_collider_component.h
@@ -3,6 +3,7 @@
 
 #include "utils.h"
 #include "core/component.h"
+#include "components.h"
 
 typedef struct {
     Component base;
@@ -10,6 +11,10 @@ typedef struct {
     Vector3 pos;
     float radius;
 } sphere_collider_component;
+
+bool is_point_inside_sphere(sphere_collider_component *comp, Vector3 point);
+bool intersect_sphere_sphere(sphere_collider_component *compA, sphere_collider_component *compB);
+bool intersect_AABB_sphere(box_collider_component *compA, sphere_collider_component *compB);
 
 sphere_collider_component *create_sphere_collider_component();
 
