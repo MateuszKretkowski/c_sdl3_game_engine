2968a0a7fba97cc5db8445489459b9a6b8392c9a
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-11-25 21:19:19 +0100

    physics_manager.c/.h: physics_manager_init(); physics_manager_add(); physics_manager_remove(); components.h: added rigid_body_component to imports;

--- src/components/components.h
+++ src/components/components.h
@@ -4,6 +4,7 @@
 #include "box_collider_component.h"
 #include "camera_component.h"
 #include "fluid_body_component.h"
+#include "rigid_body_component.h"
 #include "meshRenderer_component.h"
 #include "sample_component.h"
 #include "sphere_collider_component.h"
---
+++ src/physics/physics_manager.h
@@ -0,0 +1,9 @@
+#ifndef PHYSICS_MANAGER_H
+#define PHYSICS_MANAGER_H
+
+#include "utils/utils.h"
+#include "utils/collision_utils.h"
+#include "core/gameObject.h"
+#include "components.h"
+
+#endif
