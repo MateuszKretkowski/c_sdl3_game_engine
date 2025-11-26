2308de0ff9f7c90ef1fd8ad7b29bc804cf2f5418
Mateusz Kretkowski <mkretkowski15@gmail.com>
2025-11-26 18:55:51 +0100

    spatial_system.c/.h: adding new features;

--- src/physics/spatial_system.c
+++ src/physics/spatial_system.c
@@ -1,12 +1,38 @@
 #include "spatial_system.h"
 
 spatial_system_config spc;
+GameObject *spatial;
 
-void spatial_system_init(cJSON *json) {
+void spatial_system_realloc_objects(GameObject *objects);
+
+void spatial_system_init(GameObject *objects, int gameObject_length, cJSON *json) {
     cJSON *cell_size = cJSON_GetObjectItemCaseSensitive(json, "cell_size");
     if (!cell_size || !cell_size->valuedouble) {
         printf("spatial_system_init(): cell_size not defined.");
         return;
     }
     spc.cell_size = cell_size->valuedouble;
+    cJSON *grid_width = cJSON_GetObjectItemCaseSensitive(json, "grid_width");
+    if (!grid_width || !grid_width->valueint) {
+        printf("spatial_system_init(): grid_width not defined.");
+        return;
+    }
+    spc.grid_width = grid_width->valueint;
+    cJSON *grid_height = cJSON_GetObjectItemCaseSensitive(json, "grid_height");
+    if (!grid_height || !grid_height->valueint) {
+        printf("spatial_system_init(): grid_height not defined.");
+        return;
+    }
+    spc.grid_height = grid_height->valueint;
+
+    spatial = malloc(spc.grid_width*spc.grid_height*spc.grid_depth*sizeof(grid_cell));
+    spatial_system_realloc_objects(objects);
+}
+
+void spatial_system_realloc_objects(GameObject *objects) {
+
+}
+
+void spatial_system_update(GameObject *gameObjects) {
+
 }
