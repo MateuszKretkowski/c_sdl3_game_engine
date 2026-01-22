54aec4c5a8a1ccf27f045c874d5d338a34063a11
Mateusz Kretkowski <mkretkowski15@gmail.com>
2026-01-22 15:41:30 +0100

    camera_component: fixed bugs; transform_component: added edge case; soft_body_component: created blank space for future development; other changes are minor;

--- src/components/components.h
+++ src/components/components.h
@@ -3,10 +3,10 @@
 
 #include "box_collider_component.h"
 #include "camera_component.h"
-#include "fluid_body_component.h"
 #include "meshRenderer_component.h"
 #include "rigid_body_component.h"
 #include "sample_component.h"
+#include "soft_body_component.h"
 #include "sphere_collider_component.h"
 #include "spring_component.h"
 #include "transform_component.h"
